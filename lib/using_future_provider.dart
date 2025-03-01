2d92775729e46a7d9972c0cb0331d247b75835ec
beetana <beetana22beetana@gmail.com>
2025-03-01 20:39:07 +0900

    FutureProviderを使ってみた。

--- lib/using_future_provider.dart
+++ lib/using_future_provider.dart
@@ -1,24 +1,40 @@
 import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:riverpod_todo_tutorial/model/cat_fact.dart';
 
-final dioProvider = Provider<Dio>((ref) => Dio(BaseOptions(baseUrl: 'https://catfact.ninja/')));
-final catFactsProvider = FutureProvider<List<CatFact>>((ref) async {
-  final dio = ref.watch(dioProvider);
-  final result = await dio.get('facts');
-  final List<Map<String, dynamic>> data = List.from(result.data['data']);
-  final List<CatFact> catFactList = data.map((map) => CatFact.fromMap(map)).toList();
-  return catFactList;
-});
+final dioProvider = Provider<Dio>(
+  (ref) => Dio(BaseOptions(baseUrl: 'https://catfact.ninja/')),
+);
+
+final catFactsProvider = FutureProvider.autoDispose.family<List<CatFact>, Map<String, dynamic>>(
+  (ref, parameters) async {
+    final dio = ref.watch(dioProvider);
+    final result = await dio.get(
+      'facts',
+      queryParameters: parameters,
+    );
+    // autoDispose を使いつつ keepAlive() を呼ぶことで、一度データを取得すれば 次回画面を開いたときにキャッシュされたデータが残る。
+    // ただし、一定時間使われなかった場合には 破棄される（通常は約5分）。
+    ref.keepAlive();
+    final List<Map<String, dynamic>> data = List.from(result.data['data']);
+    final List<CatFact> catFactList = data.map((map) => CatFact.fromMap(map)).toList();
+    return catFactList;
+  },
+);
 
 class UsingFutureProvider extends ConsumerWidget {
   const UsingFutureProvider({super.key});
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    final catFacts = ref.watch(catFactsProvider);
+    final catFacts = ref.watch(
+      catFactsProvider(const {
+        'limit': 5,
+        'max_length': 50,
+      }),
+    );
+
     return Scaffold(
       appBar: AppBar(
         title: Text(
@@ -33,7 +49,7 @@ class UsingFutureProvider extends ConsumerWidget {
               itemCount: catFacts.length,
               itemBuilder: (context, index) {
                 return ListTile(
-                  title: Text(catFacts[index].fact),
+                  title: Text('$index\n${catFacts[index].fact}'),
                 );
               },
             );
